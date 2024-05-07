bcabd3b02c2e2dbc42ef7651c64ce95046d80851
Bruno Oliveira <brunootavioliveira@gmail.com>
2024-05-07 16:50:39 -0300

    Add files via upload

---
+++ Jokenpo-main/script.js
@@ -0,0 +1,30 @@
+
+function escolhaDoPC(){
+
+    const jokenpo = ['Pedra', 'Papel', 'Tesoura']
+    var numeroAleatorio = Math.floor(Math.random() * 3)
+    return jokenpo[numeroAleatorio]
+}
+
+function jogar(escolhaDoJogador){
+    var escolhaPC = escolhaDoPC()
+    var resultado = ""
+
+    if(escolhaPC === escolhaDoJogador){
+        resultado = "Deu empate"
+    }
+    else if(escolhaPC === 'Tesoura' && escolhaDoJogador === 'Papel'){
+        resultado = "O PC venceu!"
+    }
+    else if(escolhaPC === 'Papel' && escolhaDoJogador === 'Pedra'){
+        resultado = "O PC venceu!"
+    }
+    else if(escolhaPC === 'Pedra' && escolhaDoJogador === 'Tesoura'){
+        resultado = "O PC venceu!"
+    }
+    else{
+        resultado = "Você venceu!"
+    }
+    document.getElementById('escolhaPc').innerText = (`O PC escolheu: ${escolhaPC}`)
+    document.getElementById('resultado').innerText = resultado
+}
